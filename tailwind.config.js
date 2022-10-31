2f30d357344ad82686e63374507cffed20f43dc4
gokalpcayci <92229750+gokalpcayci@users.noreply.github.com>
2022-10-31 23:30:23 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "605px",
+        xl: "1440px",
+      },
+      colors: {
+        softOrange: "hsl(35, 77%, 62%)",
+        softRed: "hsl(5, 85%, 63%)",
+        offWhite: "hsl(36, 100%, 99%)",
+        grayishBlue: "hsl(233, 8%, 79%)",
+        darkGrayishBlue: "hsl(236, 13%, 42%)",
+        veryDarkBlue: "hsl(240, 100%, 5%)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
